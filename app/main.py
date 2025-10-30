efac3bfdf0568f78f6bed3dfc7d6bb1067daf740
Omer <omer_anwel@hotmail.com>
2025-10-30 14:25:04 +0500

    Fix linting issues

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ from pydantic import BaseModel
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+
 app = FastAPI()
+
 
 # Pydantic model for request validation
 class ResumeData(BaseModel):
@@ -11,9 +13,11 @@ class ResumeData(BaseModel):
     resume: str
     job_description: str
 
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Resume Screener API!"}
+
 
 @app.post("/screen_resume/")
 def screen_resume(data: ResumeData):
@@ -32,18 +36,18 @@ def screen_resume(data: ResumeData):
 
     # Compute cosine similarity between the resume and job description
     similarity_matrix = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
-    
+
     # Initialize similarity_category with a default value
     similarity_category = "Low"
 
     if similarity_matrix[0][0] > 0.8:
-     similarity_category = "High"
-    
+        similarity_category = "High"
+
     elif similarity_matrix[0][0] > 0.5:
-     similarity_category = "Medium"
-    
+        similarity_category = "Medium"
+
     else:
-     similarity_category = "Low"
+        similarity_category = "Low"
 
     # Return the similarity score as a response
     return {"similarity_score": similarity_matrix[0][0], "category": similarity_category}
